856a075edcfeb46df68226e9038064933869e5d2
Patrick <patrickalvessc@gmail.com>
2025-04-23 21:52:47 -0300

    Adicionando a classe user na aplicacao

---
+++ src/main/java/com/ead/authuser/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ead.authuser.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {}
