6914b8df9cb14971a8ac0a2c2d3f040c09d43e49
Patrick <patrickalvessc@gmail.com>
2025-04-25 16:10:50 -0300

    Modificando endpoint de usuario

--- src/main/java/com/ead/authuser/controller/UserController.java
+++ src/main/java/com/ead/authuser/controller/UserController.java
@@ -4,10 +4,7 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +28,11 @@ public class UserController {
     public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> optionalUserModel = userService.findById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(optionalUserModel.get());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        userService.delete(userService.findById(userId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
     }
 }
